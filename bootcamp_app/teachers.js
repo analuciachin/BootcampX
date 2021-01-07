fa579b42a2089d1d5169abee5fd6c5c4f040c7f9
analuciachin <ananime@gmail.com>
2021-01-07 15:02:04 +0000

    apply the parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,15 +9,19 @@ const pool = new Pool({
 
 pool.connect();
 
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`];
+
 pool.query(`
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 INNER JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
 INNER JOIN students ON assistance_requests.student_id = students.id
 INNER JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher} `);
